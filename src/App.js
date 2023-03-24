1f1a7e1568fc0e1f044a953aca88f02db5693c0e
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-03-24 23:17:44 +0530

    Revert "starting-project"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, { Fragment } from "react";
+// import { useState } from "react";
+import Header from "./Components/Layout/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
+import AvailableMeals from "./Components/Meals/AvailableMeals";
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <MealsSummary />
+        <AvailableMeals />
+      </main>
+    </Fragment>
   );
 }
 
