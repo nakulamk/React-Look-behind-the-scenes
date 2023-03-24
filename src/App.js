9c8ef71df756abf4c3b9050a4a9d4f10d339989c
Nakula Gowda <nakulamk@gmail.com>
2023-03-24 13:53:31 +0530

    starting-project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React, { Fragment } from "react";
-// import { useState } from "react";
-import Header from "./Components/Layout/Header";
-import MealsSummary from "./Components/Meals/MealsSummary";
-import AvailableMeals from "./Components/Meals/AvailableMeals";
 function App() {
   return (
-    <Fragment>
-      <Header />
-      <main>
-        <MealsSummary />
-        <AvailableMeals />
-      </main>
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
